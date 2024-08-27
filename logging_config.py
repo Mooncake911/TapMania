769ca303f854a955e3732fe086e6eb966ca560ce
shaid <shaidurovvadik@yandex.ru>
2024-08-28 00:26:35 +0300

    Commit 0.0.7

--- logging_config.py
+++ logging_config.py
@@ -1,15 +1,16 @@
 import sys
 import logging
 
-import config
+
+LOG_LEVEL = logging.DEBUG
 
 # Logging settings
 logger = logging.getLogger(__name__)
-logger.setLevel(config.LOG_LEVEL)
+logger.setLevel(LOG_LEVEL)
 
 # Logging to file
 file_handler = logging.FileHandler('hamster.log', mode='w')
-file_handler.setLevel(config.LOG_LEVEL)
+file_handler.setLevel(LOG_LEVEL)
 file_formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s -  %(threadName)s - %(message)s',
                                    datefmt='%Y-%m-%d %H:%M:%S')
 file_handler.setFormatter(file_formatter)
@@ -17,7 +18,7 @@ logger.addHandler(file_handler)
 
 # Logging to console
 console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(config.LOG_LEVEL)
+console_handler.setLevel(LOG_LEVEL)
 console_formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s -  %(threadName)s - %(message)s',
                                       datefmt='%Y-%m-%d %H:%M:%S')
 console_handler.setFormatter(console_formatter)
